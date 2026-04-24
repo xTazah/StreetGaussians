c3d65fa1f74dc8692189cf5178443a030272665d
xTazah <finnbeifong@gmail.com>
2026-04-24 18:42:27 +0200

    add run bat files for pc

---
+++ run_render_pc.bat
@@ -0,0 +1,4 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+set "LIB=%LIB%;c:\Users\koehl\anaconda3\libs"
+"D:\Git\StreetGaussians\.venv\Scripts\ns-render.exe" interpolate --load-config outputs\output_002_v2\street-gaussians-ns\2026-04-24_120250\config.yml --output-path renders\output_002_v2
